d8d8c797c9b6953824eb6a430788269f672a82d4
brandoneprice31 <bprice@college.harvard.edu>
2017-10-09 22:23:30 -0400

    starting to get going

--- db/db.py
+++ db/db.py
@@ -3,22 +3,34 @@ import datetime
 from pymongo import MongoClient
 from bson import ObjectId
 
+from config.config import Config
+
 
 class DB:
 
     def __init__(self):
-        self.db = 'blah'
+        self.config = Config()
+        self.db = MongoClient(self.config.dbURI)[self.config.dbName]
 
     def insertUser(self, user):
-
         user = {
             'first_name': user['first_name'],
             'last_name': user['last_name'],
-            'apn_token': user['apn_token'],
-            'session_token' : user['session_token']
+            'fb_token': user['fb_token'],
+            'easy_highscores': [],
+            'hard_highscores': []
         }
 
         return self.deserialize(self.db['users'].insert(user))
+
+    def findUserByFbToken(self, fbToken):
+        return self.deserialize(self.db['users'].find_one({ 'fb_token': fbToken }))
+
+    def findUserById(self, userId):
+        return self.deserialize(self.db['users'].find_one({ '_id': ObjectId(userId) }))
+
+    def updateHighScores(userId, easyHS, hardHS):
+        return self.deserialize(self.db['users'].update_one({ '_id' : ObjectId(userId) }, { '$set': { 'easy_highscores': easyHS, 'hard_highscores': hardHS } } ))
 
     # Helpers
 
--- users.py
+++ users.py
@@ -12,17 +12,52 @@ baseURI = '/' + users.name
 # POST - /users
 # {
 #   first_name: string,
-#   last_name: string
+#   last_name: string,
+#   fb_token:  string
 # }
 #
 @users.route(baseURI, methods=['POST'])
 async def postUser(request):
     body = request.json
 
-    if 'first_name' not in body and 'last_name' not in body:
+    if 'first_name' not in body and 'last_name' not in body and 'fb_token' not in body:
         return json_response({ 'error': Response.BadRequest }, status=400)
 
     user_id = db.insertUser(body)
     user = db.findUserById(user_id)
 
     return json_response({ 'user': user }, status=201)
+
+#
+# GET - /users/fb_token/:fb_token
+#
+@users.route(baseURI + '/fb_token/<fb_token>', methods=['GET'])
+async def postUser(request, fb_token):
+
+    user = db.findUserByFbToken(fb_token)
+    if user == None:
+        return json_response({ 'error': Response.NotFoundError }, status=404)
+
+    return json_response({ 'user': user }, status=200)
+
+#
+# PATCH - /users/fb_token/:fb_token/sync_highscores
+#
+@users.route(baseURI + '/fb_token/<fb_token>', methods=['PATCH'])
+async def postUser(request, fb_token):
+    body = request.json
+
+    if 'easy_highscores' not in body and 'hard_highscores' not in body:
+        return json_response({ 'error': Response.BadRequest }, status=400)
+
+    user = db.findUserByFbToken(fb_token)
+    if user == None:
+        return json_response({ 'error': Response.NotFoundError }, status=404)
+
+    hardHS = sorted(user['hard_highscores'] + body['hard_highscores'], reversed=True)[:10]
+    easyHS = sorted(user['easy_highscores'] + body['easy_highscores'], reversed=True)[:10]
+
+    db.updateHighScores(user['_id'], easyHS, hardHS)
+    user = db.findUserById(user['_id'])
+
+    return json_response({ 'user': user }, status=200)
