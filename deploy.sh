1238ddca8a497a70e0996d1bd1cd69e2adcd16f0
brandoneprice31 <bprice@college.harvard.edu>
2017-10-23 15:59:08 -0400

    automated ecr login

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,13 @@ then
   exit
 fi
 
-# aws ecr get-login
+ecr_login="$(aws ecr get-login)"
+str_to_rm=" -e none"
+eval "$(echo "$ecr_login" | sed "s@$str_to_rm@@")"
+
 docker build -t snake:$1 .;
 docker tag snake:$1 832531170141.dkr.ecr.us-east-2.amazonaws.com/snake:$1;
+
 docker push 832531170141.dkr.ecr.us-east-2.amazonaws.com/snake:$1;
 
 ~/src/ecs-deploy/./ecs-deploy -c bep-projects -n snake-$1-api -i 832531170141.dkr.ecr.us-east-2.amazonaws.com/snake:$1;
